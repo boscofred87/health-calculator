9b46ef350d6a606d7dc31f4c32b4f8ac2ea108aa
boscofred87 <boscofred87@gmail.com>
2026-08-06 14:30:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ ADMIN_PASSWORD = "admin"
 # 🎨 BACKGROUND IMAGE & GLASS UI STYLING
 # ==========================================
 # Change the web link inside the quotes below if you want a different picture!
-BACKGROUND_IMAGE_URL = "https://images.unsplash.com/photo-1557683316-973673baf926?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sb3IlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww"
+BACKGROUND_IMAGE_URL = "https://plus.unsplash.com/premium_photo-1769789060524-62f83469c231?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fGRpZ2l0YWwlMjBoZWFsdGglMjB0cmFuc3BhcmFudHxlbnwwfHwwfHx8MA%3D%3D"
 
 st.markdown(f"""
     <style>
