b63c33aa0292904e5333db54aaf49368ceb1c231
boscofred87 <boscofred87@gmail.com>
2026-08-06 17:07:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ FORMSPREEE_URL = "https://formspree.io/f/mvkpolar"
 # ==========================================
 # 🎨 BACKGROUND IMAGE & GLASS UI STYLING
 # ==========================================
-BACKGROUND_IMAGE_URL = "https://images.unsplash.com/photo-1622230208571-095e610ff4da?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGdyZWVuJTIwd2hpdGUlMjBkaWdpdGFsJTIwaGVhbHRofGVufDB8fDB8fHww"
+BACKGROUND_IMAGE_URL = "https://plus.unsplash.com/premium_photo-1771253963523-5fd863e93375?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGdyZWVuJTIwd2hpdGV8ZW58MHx8MHx8fDA%3D"
 
 st.markdown(f"""
     <style>
